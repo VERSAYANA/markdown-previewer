14ce7754e2695461b9a7d41a32b686d958e4696f
Ramtin Khalatbari <VERSAYANA@outlook.com>
2018-09-01 18:53:16 +0430

    Added initialInput

--- src/App.js
+++ src/App.js
@@ -2,14 +2,15 @@ import React, { Component } from 'react';
 import marked from 'marked';
 import Editor from './components/Editor/Editor';
 import Previewer from './components/Previewer/Previewer';
+import initialInput from './initialInput';
 import './App.css';
 
 class App extends Component {
   constructor(props) {
     super(props);
     this.state = {
-      input: '',
-      preview: '',
+      input: initialInput,
+      preview: marked(initialInput),
     };
     this.onEditorChange = this.onEditorChange.bind(this);
   }
---
+++ src/initialInput.js
@@ -0,0 +1,48 @@
+const input = `# Markdown Previewer 
+----
+## What is Markdown?
+
+![markdownlogo](https://upload.wikimedia.org/wikipedia/commons/4/48/Markdown-mark.svg)
+
+see [Wikipedia](http://en.wikipedia.org/wiki/Markdown)
+
+> Markdown is a lightweight markup language, originally created by John Gruber and Aaron Swartz allowing people "to write using an easy-to-read, easy-to-write plain text format, then convert it to structurally valid XHTML (or HTML)".
+
+----
+
+## markdown quick reference
+# headers
+
+*emphasis*
+
+**strong**
+
+~~crossing text~~
+
+* list
+
+\`<p>Inline Code</p>\`
+
+\`\`\`
+// this is multi-line code:
+
+function anotherExample(firstLine, lastLine) {
+  if (firstLine == '\`\`\`' && lastLine == '\`\`\`') {
+    return multiLineCode;
+  }
+}
+\`\`\`
+
+And if you want to get really crazy, even tables:
+
+Wild Header | Crazy Header | Another Header?
+------------ | ------------- | ------------- 
+Your content can | be here, and it | can be here....
+And here. | Okay. | I think we get it.
+
+[links](http://wikipedia.org)
+
+>block quote
+`;
+
+export default input;
